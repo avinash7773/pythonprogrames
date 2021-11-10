6d63a956defdb3f93598c2a469842e1d561c4cb5
niku098 <javinash228@gmail.com>
2021-11-10 10:37:10 +0530

    Refactor : flipCoin programe

--- flipcoin.py
+++ flipcoin.py
@@ -5,25 +5,38 @@
 @Title : find head and tail percentage by using random() function
 '''
 
-tail_count = 0;
-head_count = 0;
-import random 
+import random
 
-number_of_times_flip_coin = int(input("Enter any number:"))
-
-if(number_of_times_flip_coin <= 10):
-    print("number_of_times_flip_coin should be positive")
-else:
-    i = 0
-    while(i < number_of_times_flip_coin ):
-        random_number = random.random()
-        if(random_number < 0.5) :
-            tail_count = tail_count + 1
-        else:
-            head_count = head_count + 1
-        i = i + 1
-    tail_percentage = (tail_count / number_of_times_flip_coin) * 100
-    head_percentage = (head_count / number_of_times_flip_coin) * 100
+'''
+@Description : flipcoin method will print percentage of tail and head
+@Parameter   : parameter is only one that is number of time coin flip
+@return      : return tail percentage and head percentage '''
+def flipCoin(number_of_times_flip_coin):
+   if(number_of_times_flip_coin <= 0):
+        print("number_of_times_flip_coin should be positive")
+   else:
+         tail_count = 0;
+         head_count = 0;
+         i = 0
+         while(i < number_of_times_flip_coin ):
+            random_number = random.random()
+            if(random_number <= 0.5) :
+                tail_count = tail_count + 1
+            else:
+                head_count = head_count + 1
+            i = i + 1
+         tail_percentage = (tail_count / number_of_times_flip_coin) * 100
+         head_percentage = (head_count / number_of_times_flip_coin) * 100
+         return tail_percentage, head_percentage
+          
+try:
+    number_of_times_flip_coin = int(input("Enter any number:"))
+    tail_percentage,head_percentage = flipCoin(number_of_times_flip_coin)
     print("Tail Percentage=",tail_percentage,"%")
     print("head percentage=",head_percentage,"%")
+except Exception as e:
+    print("input should be integer",e)
+
+   
+
 
