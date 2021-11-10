db71cefd66328a609010c9e5e0a8db91784b83c8
niku098 <javinash228@gmail.com>
2021-11-10 10:46:33 +0530

    Refactor : Leap year programe

--- leapyear.py
+++ leapyear.py
@@ -5,19 +5,26 @@
 @Title : check year is leap or not
 '''
 
+'''
+@Description : isLeap function will check given year is leap or not
+@Parameter   : parameter is only one that is year
+@return      : return boolean  '''
 def isLeap(year) :
     if((year % 400 == 0) or (year % 100 != 0) and (year % 4 == 0)):
         return True
     else:
         return False
-    
 
-year = int(input("Enter year:"))
+try:
+    year = int(input("Enter year:"))
+    if(isLeap(year)):
+        print(year," is Leap Year")
+    else:
+        print(year," is not Leap Year")
+except ValueError as e:
+    print("year should be integer",e)
 
-if(isLeap(year)):
-    print(year," is Leap Year")
-else:
-    print(year," is not Leap Year")
+
 
 
     
