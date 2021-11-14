443f793c7abaa8380e06673c32606fad297703eb
niku098 <javinash228@gmail.com>
2021-11-14 10:16:06 +0530

    Refactor : sumoftriplets equal to zero programe

--- functionalPrograme/sumoftriplets.py
+++ functionalPrograme/sumoftriplets.py
@@ -1,4 +1,17 @@
+"""
+@Author : Avinash Jadhav
+@Date : 2021-11-11
+@Last Modify by : Avinash Jadhav
+@Title : print three integer number
+"""
+
+
 def find_triplets(arr, n):
+    """
+        @Description : find triplets which sum equal to zero
+        @Parameter   : parameter is only one that is array
+        @return      : -
+        """
     found = False
     for i in range(0, n - 2):
         for j in range(i + 1, n - 1):
@@ -6,7 +19,7 @@ def find_triplets(arr, n):
                 if(arr[i] + arr[j] + arr[k] == 0):
                     print(arr[i], arr[j], arr[k])
                     found = True
-    if(found == False):
+    if found == False:
         print(" not exist ")
 
 
